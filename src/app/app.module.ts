929f75d04cd039c3f672574eeceaf1846db6b247
akaushik95 <ashukaushik8395@gmail.com>
2017-10-30 12:45:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageViewComponent } from './image-view/image-view.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageViewComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
